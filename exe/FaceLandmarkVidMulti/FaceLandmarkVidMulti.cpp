51152404c17cca7b3fdb1f49e0a7e9e36f7532e2
Torben Toeniges <ttoenige@techfak.uni-bielefeld.de>
2016-05-18 14:38:22 +0200

    resolve tbb include error

--- exe/FaceLandmarkVidMulti/FaceLandmarkVidMulti.cpp
+++ exe/FaceLandmarkVidMulti/FaceLandmarkVidMulti.cpp
@@ -69,6 +69,10 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+// TBB includes
+#include <tbb/atomic.h>
+#include <tbb/parallel_for.h>
+
 #define INFO_STREAM( stream ) \
 std::cout << stream << std::endl
 
